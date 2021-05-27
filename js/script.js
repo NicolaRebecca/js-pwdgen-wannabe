417cb485d2bd31d09d4e6a7e67073075e739dc81
NicolaRebecca <master9895@gmail.com>
2021-05-27 13:46:12 +0200

    script aggiunto

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var nome = prompt ("ciao,inserisci il nome");
+var cognome = prompt ("inserisci il cognome");
+var colore = prompt ("inserisci colore preferito")
+
+document.getElementById ("password").innerHTML = nome + cognome + colore + "21"
